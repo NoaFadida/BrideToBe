fbcc9ce1958b41cf2118dd47edba19ab16239df9
Noa Fadida <noafadida122@gmail.com>
2022-03-14 01:07:32 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,76 +1,64 @@
 // Requiring modules
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-var fs = require('fs');
+const ejs = require("ejs");
+var fs = require("fs");
 
 const port = 4000;
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 // app.use(express.static(__dirname + '/public'));
 // Render index.ejs file
-app.get('/', function (req, res) {
-   
-    // Render page using renderFile methodsdsdsss
-    ejs.renderFile('view/home.ejs', {}, 
-        {}, function (err, template) {
-        if (err) {
-            throw err;
-        } else {
-            res.end(template);
-        }
-    });
+app.get("/", function (req, res) {
+  // Render page using renderFile methodsdsdsss
+  ejs.renderFile("view/home.ejs", {}, {}, function (err, template) {
+    if (err) {
+      throw err;
+    } else {
+      res.end(template);
+    }
+  });
 });
 
 // Server setup
 app.listen(port, function (error) {
-    if (error)
-        throw error;
-    else
-        console.log("Server is running");
+  if (error) throw error;
+  else console.log("Server is running");
 });
 
-app.get('/Signin', function (req, res) {
-   
-    // Render page using renderFile methodsdsdsss
-    ejs.renderFile('view/signin.ejs', {}, 
-        {}, function (err, template) {
-        if (err) {
-            throw err;
-        } else {
-            res.end(template);
-        }
-    });
+app.get("/Signin", function (req, res) {
+  // Render page using renderFile methodsdsdsss
+  ejs.renderFile("view/signin.ejs", {}, {}, function (err, template) {
+    if (err) {
+      throw err;
+    } else {
+      res.end(template);
+    }
+  });
 });
 
-app.get('/Signup', function (req, res) {
-   
-    // Render page using renderFile methodsdsdsss
-    ejs.renderFile('view/signup.ejs', {}, 
-        {}, function (err, template) {
-        if (err) {
-            throw err;
-        } else {
-            res.end(template);
-        }
-    });
+app.get("/Signup", function (req, res) {
+  // Render page using renderFile methodsdsdsss
+  ejs.renderFile("view/signup.ejs", {}, {}, function (err, template) {
+    if (err) {
+      throw err;
+    } else {
+      res.end(template);
+    }
+  });
 });
-app.get('/Help', function (req, res) {
-   
-    // Render page using renderFile methodsdsdsss
-    ejs.renderFile('view/help.ejs', {}, 
-        {}, function (err, template) {
-        if (err) {
-            throw err;
-        } else {
-            res.end(template);
-        }
-    });
+app.get("/Help", function (req, res) {
+  // Render page using renderFile methodsdsdsss
+  ejs.renderFile("view/help.ejs", {}, {}, function (err, template) {
+    if (err) {
+      throw err;
+    } else {
+      res.end(template);
+    }
+  });
 });
 
 // Server setup
 app.listen(port, function (error) {
-    if (error)
-        throw error;
-    else
-        console.log("Server is running");
+  if (error) throw error;
+  else console.log("Server is running");
 });
