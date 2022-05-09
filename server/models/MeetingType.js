c5b0fcf15c4344294816b0e4cb0220b9f3dcf1f8
Noa Fadida <noafadida122@gmail.com>
2022-05-09 10:49:55 +0300

    MeetingType  schema

--- server/models/MeetingType.js
+++ server/models/MeetingType.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 const MeetingTypes = new mongoose.Schema({
-
+    serviceProviderId: {
+        type: String,
+        required: true,
+    },
     TypeOfMeeting: {
-        
-        type: Object,
-        enum: [{ "initial consulting": 60 },
-            {"fitting": 40},
-            {"pick up dress": 30 }],
+        type: String,
         require: true
     },
 }, { timestamps: true });
