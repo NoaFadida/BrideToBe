b23cd1b2650f5fb954327eb1ff99955b17875026
Noa Fadida <noafadida122@gmail.com>
2022-05-09 11:37:10 +0300

    Fix ContactUs

--- client/src/components/common/ContactUs.jsx
+++ client/src/components/common/ContactUs.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { IconButton } from "@material-ui/core"
-import { EmailOutlined } from "@material-ui/icons"
-import "./ContactUs.scss"
-
-
+import { IconButton } from "@material-ui/core";
+import { EmailOutlined } from "@material-ui/icons";
+import './ContactUs.scss';
 
 const ContactUs = () => {
 
