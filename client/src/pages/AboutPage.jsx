218bff17773bdfc4ce8182f80b2231102b34a7cd
AmitGodosi <94754700+AmitGodosi@users.noreply.github.com>
2022-05-25 00:31:04 +0300

    update about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Announcment from "../components/Announcment/Announcment";
 import Navbar from "../components/Navbar/Navbar";
 import AboutUs from "../components/AboutUs/AboutUs";
-import "./main.scss";
 
 const AboutPage = () => {
   return (
-    <div className="pages-container">
+    <>
       <Announcment />
       <Navbar />
       <AboutUs />
-    </div>
+    </>
   );
 };
 
